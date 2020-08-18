ca70f02eafcc03a4b9de4634f569e24240a6efca
Veso <Ambroza8888@gmail.com>
2020-08-17 23:29:22 -0700

    't'

---
+++ apps/express-app-3/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'express-app-3',
+  preset: '../../jest.config.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/express-app-3',
+};
