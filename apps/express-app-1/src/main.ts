4b9a297e5aa5a6dd979c3e98e7c58ade507723c2
Veso <Ambroza8888@gmail.com>
2020-08-19 13:20:38 -0700

    qew

--- apps/express-app-1/src/main.ts
+++ apps/express-app-1/src/main.ts
@@ -4,17 +4,15 @@
  */
 
 import * as express from 'express';
-
+import { environment } from './environments/environment';
 const app = express();
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to express-app-1!' });
 });
 
-const port = 8080;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+app.set('port', process.env.PORT || environment.PORT || 3000);
+const server = app.listen(app.get('port'), function () {
+  console.log('KT3 profile API Express server listening on port %d in %s mode', app.get('port'), app.settings.env);
 });
 server.on('error', console.error);
-
-// rrr
--- apps/express-app-3/src/main.ts
+++ apps/express-app-3/src/main.ts
@@ -2,8 +2,8 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
 import * as express from 'express';
+import { environment } from './environments/environment';
 
 const app = express();
 
@@ -11,8 +11,8 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to express-app-3!' });
 });
 
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+app.set('port', process.env.PORT || environment.PORT || 3000);
+const server = app.listen(app.get('port'), function () {
+  console.log('KT3 profile API Express server listening on port %d in %s mode', app.get('port'), app.settings.env);
 });
 server.on('error', console.error);
