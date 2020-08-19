946c24c5a94714f1838f3ab7e7f25b1b4aa1c981
Veso <Ambroza8888@gmail.com>
2020-08-19 12:05:45 -0700

    't'

---
+++ apps/express-app-4/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'express-app-4',
+  preset: '../../jest.config.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/express-app-4',
+};
---
+++ apps/express-app-4/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  PORT: 3341,
+  CORS_ORIGINS: [
+    'http://localhost:4200',
+    'capacitor://localhost',
+    'ionic://localhost',
+    'http://localhost',
+  ],
+  ROOT_URL: 'http://localhost:3341/',
+};
