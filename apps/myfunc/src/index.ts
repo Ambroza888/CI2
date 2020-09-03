22e8f78b1f96c15a557114505a15849f915174d9
Veso <Ambroza8888@gmail.com>
2020-09-03 13:56:44 -0700

    qweqwe

---
+++ apps/myfunc/src/index.ts
@@ -0,0 +1,16 @@
+export const myfunc = async (req: any, res?: any) => {
+  const auth = req.query.apiSecret === process.env.apiSecret;
+  console.log('1');
+  if (auth) {
+    try {
+      const value = req.query.value || 'Hello World';
+      return res.status(200).send(value);
+    } catch (error) {
+      console.error(new Error(error));
+      return res.sendStatus(500);
+    }
+  } else {
+    console.error(new Error(`401`));
+    return res.sendStatus(401);
+  }
+};
